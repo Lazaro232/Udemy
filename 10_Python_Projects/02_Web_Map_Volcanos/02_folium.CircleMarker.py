b0311e66503aa14cbfcd69385aaf3424952441de
Lazaro232 <joselazaroneto97@gmail.com>
2021-07-20 18:03:49 -0300

    Adding Web Map of Volcanoes and Population of the world

---
+++ 10_Python_Projects/02_Web_Map_Volcanos/02_folium.CircleMarker.py
@@ -0,0 +1,44 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+latitude_list = list(data["LAT"])
+longitude_list = list(data["LON"])
+elevation_list = list(data["ELEV"])
+# Usado para estilizar o POPUP do marcador
+HTML = """<h4>Volcano information:</h4>
+Height: %s m
+"""
+
+
+def set_marker_color(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+map = folium.Map(location=[38.58, -99.09],
+                 zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+
+for latitude, longitude, elevation in zip(latitude_list,
+                                          longitude_list,
+                                          elevation_list):
+    iframe = folium.IFrame(html=HTML % str(elevation),
+                           width=200,
+                           height=100)
+
+    fg.add_child(folium.CircleMarker(location=[latitude, longitude],
+                                     radius=10,
+                                     popup=folium.Popup(iframe),
+                                     fill_color=set_marker_color(elevation),
+                                     color='grey',
+                                     fill_opacity=0.7))
+
+
+map.add_child(fg)
+map.save("Map1.html")
