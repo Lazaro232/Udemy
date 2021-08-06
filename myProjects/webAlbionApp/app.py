bc5d5e64d459416b2312889ddb980ff9f31b8fcf
Lazaro232 <joselazaroneto97@gmail.com>
2021-08-06 15:44:33 -0300

    Improving app.py to work with any food

--- myProjects/webAlbionApp/app.py
+++ myProjects/webAlbionApp/app.py
@@ -23,39 +23,53 @@ from math import floor
 meal = OpenMarket()
 
 
-def omelette_t3():
-    # Albion informations
-    (omelette, wheat, chicken, hen) = meal.omelette_t3()
-    omelette_price = omelette[1]['price']
-    wheat_price = wheat[1]['price']
-    chicken_price = chicken[1]['price']
-    hen_price = hen[1]['price']
-
-    # User information
+def get_user_info():
     amount_to_craft = float(input('Quantidade a ser fabricada: '))
     tax_fee = float(input('Taxa da loja a ser utilizada: '))/100
     focus = int(input('1: Com foco --- 0: Sem foco '))
+    return amount_to_craft, tax_fee, focus
 
-    # Calculations
-    tax_to_craft = tax_fee * 5 * ItemValue.OMELETTE["T3"]
 
+def get_food_info(omelette_info):
+    resources_info = []
+    for i in range(len(omelette_info)):
+        if i == 0:
+            food_price, = omelette_info[i].keys()
+            item_value, = omelette_info[i].values()
+            food_info = [food_price, item_value]
+        else:
+            resource_price, = omelette_info[i].keys()
+            resource_amount, = omelette_info[i].values()
+            resources_info_list = [resource_price, resource_amount]
+            resources_info.append(resources_info_list)
+    return [food_info, resources_info]  # [[], [ [], [], [] ...] ]
+
+
+def calculations(food_info):
+    (amount_to_craft, tax_fee, focus) = get_user_info()
+    food_price = food_info[0][0]
+    item_value = food_info[0][1]
+
+    tax_to_craft = tax_fee * 5 * item_value
     if focus:
         return_rate = 0.453
     else:
         return_rate = 0.152
-
     real_return_rate = 1 + return_rate + \
         return_rate**2 + return_rate**3 + return_rate**4
 
     amount_crafted = floor(amount_to_craft * real_return_rate / 10)*10
-    material_cost = amount_to_craft/10 * (wheat_price * Foods.OMELETTE['T3']['wheat'] +
-                                          chicken_price * Foods.OMELETTE['T3']['chicken'] +
-                                          hen_price * Foods.OMELETTE['T3']['hen'])
+
+    resource_cost = 0
+    for resource_info in food_info[1]:
+        resource_cost += resource_info[0] * resource_info[1]
+
+    material_cost = amount_to_craft/10 * resource_cost
     craft_cost = amount_crafted * tax_to_craft / 10
-    sell_cost = omelette_price * Fees.MARKET_FEE * amount_crafted
+    sell_cost = food_price * Fees.MARKET_FEE * amount_crafted
     total_cost = material_cost + craft_cost + sell_cost
 
-    gross_profit = omelette_price * amount_crafted
+    gross_profit = food_price * amount_crafted
     result = gross_profit - total_cost
     final_result = abs(floor(result))
 
@@ -65,4 +79,25 @@ def omelette_t3():
         return "Lucro de {:,} pratas".format(final_result).replace(',', '.')
 
 
-print(omelette_t3())
+def omelette_t3():
+    # Albion informations
+    (omelette, wheat, chicken, hen) = meal.omelette_t3()
+    omelette_price = omelette[1]['price']
+    wheat_price = wheat[1]['price']
+    chicken_price = chicken[1]['price']
+    hen_price = hen[1]['price']
+
+    omelette_info = [
+        {omelette_price: ItemValue.OMELETTE["T3"]},
+        {wheat_price: Foods.OMELETTE['T3']['wheat']},
+        {chicken_price: Foods.OMELETTE['T3']['chicken']},
+        {hen_price: Foods.OMELETTE['T3']['hen']}
+    ]
+
+    food_info = get_food_info(omelette_info)
+
+    print(calculations(food_info))
+
+
+# print(omelette_t3())
+omelette_t3()
