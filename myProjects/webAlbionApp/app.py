b219e9fb6b5b02ed86d0588100bca1f32cdebe53
Lazaro232 <joselazaroneto97@gmail.com>
2021-08-06 16:25:35 -0300

    Improving app.py to work in all cities

--- myProjects/webAlbionApp/app.py
+++ myProjects/webAlbionApp/app.py
@@ -24,44 +24,38 @@ meal = OpenMarket()
 
 
 def get_user_info():
-    amount_to_craft = float(input('Quantidade a ser fabricada: '))
-    tax_fee = float(input('Taxa da loja a ser utilizada: '))/100
+    amount_to_craft = float(input('Quantidade a ser fabricada (e.g, 100): '))
+    tax_fee = float(input('Taxa da loja a ser utilizada (e.g, 50): '))/100
     focus = int(input('1: Com foco --- 0: Sem foco '))
     return amount_to_craft, tax_fee, focus
 
 
-def get_food_info(omelette_info):
-    resources_info = []
-    for i in range(len(omelette_info)):
-        if i == 0:
-            food_price, = omelette_info[i].keys()
-            item_value, = omelette_info[i].values()
-            food_info = [food_price, item_value]
-        else:
-            resource_price, = omelette_info[i].keys()
-            resource_amount, = omelette_info[i].values()
-            resources_info_list = [resource_price, resource_amount]
-            resources_info.append(resources_info_list)
-    return [food_info, resources_info]  # [[], [ [], [], [] ...] ]
-
-
-def calculations(food_info):
-    (amount_to_craft, tax_fee, focus) = get_user_info()
+def calculations(user_info: list, food_info: list, city: str):
+    amount_to_craft = user_info[0]
+    tax_fee = user_info[1]
+    focus = user_info[2]
     food_price = food_info[0][0]
     item_value = food_info[0][1]
 
     tax_to_craft = tax_fee * 5 * item_value
     if focus:
-        return_rate = 0.453
+        if city == 'Caerleon':
+            return_rate = 0.479
+        else:
+            return_rate = 0.435
     else:
-        return_rate = 0.152
+        if city == 'Caerleon':
+            return_rate = 0.248
+        else:
+            return_rate = 0.152
+
     real_return_rate = 1 + return_rate + \
         return_rate**2 + return_rate**3 + return_rate**4
 
     amount_crafted = floor(amount_to_craft * real_return_rate / 10)*10
 
     resource_cost = 0
-    for resource_info in food_info[1]:
+    for resource_info in food_info[1:]:
         resource_cost += resource_info[0] * resource_info[1]
 
     material_cost = amount_to_craft/10 * resource_cost
@@ -72,32 +66,37 @@ def calculations(food_info):
     gross_profit = food_price * amount_crafted
     result = gross_profit - total_cost
     final_result = abs(floor(result))
+    result_str = "de {:,} pratas".format(final_result).replace(',', '.')
 
     if result < 0:
-        return "Prejuízo de {:,} pratas".format(final_result).replace(',', '.')
+        return f"{city}: Prejuízo {result_str}"
     else:
-        return "Lucro de {:,} pratas".format(final_result).replace(',', '.')
+        return f"{city}: Lucro {result_str}"
 
 
 def omelette_t3():
     # Albion informations
     (omelette, wheat, chicken, hen) = meal.omelette_t3()
-    omelette_price = omelette[1]['price']
-    wheat_price = wheat[1]['price']
-    chicken_price = chicken[1]['price']
-    hen_price = hen[1]['price']
+    # User informations
+    (amount_to_craft, tax_fee, focus) = get_user_info()
+    user_info = [amount_to_craft, tax_fee, focus]
 
-    omelette_info = [
-        {omelette_price: ItemValue.OMELETTE["T3"]},
-        {wheat_price: Foods.OMELETTE['T3']['wheat']},
-        {chicken_price: Foods.OMELETTE['T3']['chicken']},
-        {hen_price: Foods.OMELETTE['T3']['hen']}
-    ]
+    for city_info in range(len(omelette)):
+        omelette_price = omelette[city_info]['price']
+        wheat_price = wheat[city_info]['price']
+        chicken_price = chicken[city_info]['price']
+        hen_price = hen[city_info]['price']
+        city = omelette[city_info]['city']
 
-    food_info = get_food_info(omelette_info)
+        omelette_info = [
+            [omelette_price, ItemValue.OMELETTE["T3"]],
+            [wheat_price, Foods.OMELETTE['T3']['wheat']],
+            [chicken_price, Foods.OMELETTE['T3']['chicken']],
+            [hen_price, Foods.OMELETTE['T3']['hen']]
+        ]
 
-    print(calculations(food_info))
+        # Calculations
+        print(calculations(user_info, omelette_info, city))
 
 
-# print(omelette_t3())
 omelette_t3()
